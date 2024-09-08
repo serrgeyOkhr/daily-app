6c42b666a4e05acf2b69c1a57373a6a419914b63
Sergey <sergey@ohryamkin.ru>
2024-09-08 11:10:25 +0700

    add configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  plugins: ["@typescript-eslint", "import"],
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    "plugin:@typescript-eslint/recommended",
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting',
+    "@vue/prettier",
+    "@vue/typescript",
+    "plugin:vue-scoped-css/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
